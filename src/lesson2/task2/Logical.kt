e1224cd3498593532ee203a5105fe8e0f10b0ab9
galahad <asj931@yahoo.com>
2017-10-06 12:00:11 +0300

    2l 1t

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -58,5 +58,5 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * Вернуть true, если кирпич пройдёт
  */
 fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean =
-        (a<=r&&b<=s)||(a<=s&&b<=r)||(b<=r&&c<=s)||(b<=s&&c<=r)||(a<=r&&c<=s)||(a<=s&&c<=r)
+        (a<=r&&b<=s)||(a<=s&&b<=r)||(b<=r&&c<=s)||(b<=s&&c<=r)||(a<=r&&c<=s) || (a<=s&&c<=r)
 
