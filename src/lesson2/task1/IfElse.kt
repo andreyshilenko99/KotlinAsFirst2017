f84ddc23aa499f44e23e2db10f3556278a6d7ff9
galahad <asj931@yahoo.com>
2017-10-06 10:26:15 +0300

    2l 1t

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -124,10 +124,10 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Если пересечения нет, вернуть -1.
  */
 fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int  {
-   if ((b>a)&&(d>c)&&(c>b)) return -1
+   if ((b>=a)&&(d>c)&&(c>b)) return -1
     if ((c>a)&&(b>=c)&&(d>b)) return (b-c)
     if((c>a)&&(d>=c)&&(b>d)) return (d-c)
-    if ((a>c)&&(d>=a)&&(b>d)) return (d-a)
+    if ((a>c)&&(d>a)&&(b>d)) return (d-a)
     if ((a>d)&&(a>c)) return -1
     if ((a>c)&&(d>b)&&(b>=a)) return (b-a)
     if ((a==b)||(d==c)) return 0
